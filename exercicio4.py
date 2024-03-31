22bb8a072d8d8d5f80f22204af229a44e30658dd
Douglas Lima <165210202+limadouglas07@users.noreply.github.com>
2024-03-31 20:28:21 -0300

    Update exercicio4.py
    Foi colocado uma quebra de linha

--- exercicio4.py
+++ exercicio4.py
@@ -1,4 +1,5 @@
-#1- Escreva um algoritmo que solicite um número ao usuário. Caso seja digitado um valor entre 0 e 9, mostre: “valor #correto”, caso contrário mostre: “valor incorreto.
+#1- Escreva um algoritmo que solicite um número ao usuário. Caso seja digitado um valor entre 0 e 9, mostre: “valor #correto”,
+# caso contrário mostre: “valor incorreto.
 
 n = int(input("Digite aqui um número:"))
 
