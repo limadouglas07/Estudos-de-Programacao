c89f123a0a7745a2f7a75da427a3be7e680667c5
Douglas Lima <lima.douglas08@gmail.com>
2024-04-04 08:41:35 -0300

    Update ExercicioAula5.py
    exercicio de parcelas

---
+++ Codigos_python/ExercicioAula5.py
@@ -0,0 +1,17 @@
+totalcompra = float(input("Digite o valor total da compra:"))
+parcelas = int(input("Digite aqui o número de parcelas 2/4/6/8:"))
+
+if parcelas == 2:
+    total = (totalcompra * 1.03)/2
+    print("O valor das duas parcelas de {} é R$ {}".format(parcelas, total))
+elif parcelas == 4:
+    total = (totalcompra * 1.07)/4
+    print("O valor das duas parcelas de {} é R$ {}".format(parcelas, total))
+elif parcelas == 6:
+    total = (totalcompra * 1.09)/6
+    print("O valor das duas parcelas de {} é R$ {}".format(parcelas, total))
+elif parcelas == 8:
+    total = (totalcompra * 1.12)/8
+    print("O valor das duas parcelas de {} é R$ {}".format(parcelas, total))
+else:
+    print("Dados inválidos!!!!")
