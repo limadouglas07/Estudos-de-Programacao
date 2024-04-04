f039fe6701a50bef63a79e2fc974ec464149b057
limadouglas07 <lima.douglas08@gmail.com>
2024-04-04 14:53:13 -0300

    Update ExercicioLacoFor.py

--- Codigos_python/ExercicioLacoFor.py
+++ Codigos_python/ExercicioLacoFor.py
@@ -2,7 +2,7 @@
 animais =[ 'gato','cachorro','leão','camelo']
 for a in animais:
     print(a,"string de tamanho", len(a))
-    
+        
 sequencia = [0,1,2,3,4,5]
 for num in sequencia:
     print(num)
