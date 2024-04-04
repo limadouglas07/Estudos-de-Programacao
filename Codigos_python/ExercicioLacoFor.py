af2a624098bd4735e49032a0265c768cf31ce82b
limadouglas07 <lima.douglas08@gmail.com>
2024-04-04 14:49:16 -0300

    Create ExercicioLacoFor.py

---
+++ Codigos_python/ExercicioLacoFor.py
@@ -0,0 +1,17 @@
+# Laço For
+animais =[ 'gato','cachorro','leão','camelo']
+for a in animais:
+    print(a,"string de tamanho", len(a))
+    
+sequencia = [0,1,2,3,4,5]
+for num in sequencia:
+    print(num)
+    
+for i in range(10):
+    print(i, end=" ")
+
+for i in range (3,8):
+    print(i,end=" ")
+    
+for i in range (0,21,2):
+    print(i, end =" ")
