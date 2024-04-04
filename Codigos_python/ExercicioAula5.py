6f404a744aea7b507a79f42ccbdedb5a94b4e7e1
Douglas Lima <lima.douglas08@gmail.com>
2024-04-04 10:33:12 -0300

    Update ExercicioAula5.py

--- Codigos_python/ExercicioAula5.py
+++ Codigos_python/ExercicioAula5.py
@@ -34,3 +34,22 @@ elif digito == 7 or digito == 8:
 else:
     rodizio = "Sexta-Feira"
 print("Este veículo está com rodízio na {} ".format(rodizio))
+
+
+# Laço For
+animais =[ 'gato','cachorro','leão','camelo']
+for a in animais:
+    print(a,"string de tamanho", len(a))
+    
+sequencia = [0,1,2,3,4,5]
+for num in sequencia:
+    print(num)
+    
+for i in range(10):
+    print(i, end=" ")
+
+for i in range (3,8):
+    print(i,end=" ")
+    
+for i in range (0,21,2):
+    print(i, end =" ")
