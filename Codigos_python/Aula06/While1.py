8932438092107afd8daf4a32e527f648abd78d6d
limadouglas07 <lima.douglas08@gmail.com>
2024-04-11 17:58:28 -0300

    Update While1.py

--- Codigos_python/Aula06/While1.py
+++ Codigos_python/Aula06/While1.py
@@ -5,7 +5,7 @@ soma = 0
 while num!= -1:
     num = float(input("Digite um número (-1) para sair:"))
     if num != -1:
-     soma+=num
+     soma+= num
      contador+= 1
 media = soma/contador
 print("A média é {:.2f}".format(media))
