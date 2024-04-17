c07022c83b86996df24889b9883d9efd4bfc8bf0
limadouglas07 <lima.douglas08@gmail.com>
2024-04-17 17:54:11 -0300

    Update While.Contador.py
    mudança nas variáveis

--- Codigos_python/Aula06/While.Contador.py
+++ Codigos_python/Aula06/While.Contador.py
@@ -1,7 +1,8 @@
 contador = 0
 
-while contador < 100:
+while contador < 45:
     contador += 1
+
     if contador == 6:
         print("Não vou mostrar o número 6..")
         continue
@@ -9,6 +10,7 @@ while contador < 100:
     if contador >= 10 and contador <= 27:
         print("Não vou mostrar", contador)
         continue
+
     print(contador)
     if contador == 40:
         break
