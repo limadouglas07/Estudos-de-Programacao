da23765e04387bf9c949c6ab7fa9c0c43d769725
Douglas Lima <lima.douglas08@gmail.com>
2024-04-25 09:47:08 -0300

    Update ExercicioAula8.py

--- Codigos_python/Aula08/ExercicioAula8.py
+++ Codigos_python/Aula08/ExercicioAula8.py
@@ -1,1 +1,10 @@
-print("Testando!!!! ")
+print('Imprimindo com aspas simples.')
+print("Imprimindo com aspas duplas")
+print("""Imprimindo com aspas triplas
+      assim posso pular linhas,
+      varias linhas!!!""")
+print("Com aspas duplas podemos\n podemos quebrar as linhas")
+
+fruta = "banana"
+letra = fruta[1]
+print('O caractere na posição 1 de banana é', letra)
