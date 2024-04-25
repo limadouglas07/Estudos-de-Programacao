114eca75b63d2e628f27561ea60bb6196542a2a1
Douglas Lima <lima.douglas08@gmail.com>
2024-04-25 09:47:00 -0300

    Create Variaveisstring.py

---
+++ Codigos_python/Aula08/Variaveisstring.py
@@ -0,0 +1,8 @@
+texto = input("Digite um número inteiro:")
+tamanho = len(texto)
+print('O número informado tem {} digitos'.format(tamanho))
+
+numero = int(texto)
+tamanho = len(str(numero))
+print("O número informado tem {} digitos".format(tamanho))
+
