41986f4476c57d3504d026002a2e854c9373b5ee
Douglas Lima <lima.douglas08@gmail.com>
2024-04-25 11:05:24 -0300

    Update OperacoesString.py

--- Codigos_python/Aula08/OperacoesString.py
+++ Codigos_python/Aula08/OperacoesString.py
@@ -1,3 +1,4 @@
-valor = float(input("Digite o valor da compra"))
-percentual = 1.05 # Valor de 5% acrescido.
-print("O valor acrescido é de R${:.2f}".format(valor*percentual))
+valor = float(input("Digite o valor da compra:"))
+percentual = float(input("qual o valor do percentual:"))
+valornovo= valor*(1+percentual/100)
+print("O valor R${:.2f} com acréscimo de {:.1f}% é de R$ {:.2f}".format(valor, percentual,valornovo))
